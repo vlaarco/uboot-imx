c31ba1f4ce8edd38c9e140e0c3e627ba928f6932
Peng Fan <peng.fan@nxp.com>
2024-07-11 10:37:23 +0800

    LFU-760 configs: imx95: enlarge dom0 mem
    2048MB is too small for virtio based gpu, vhost-user solution. With
    virtio, the memory will be allocated from dom0, so enlarge it to 4GB
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- include/configs/verdin-imx95.h
+++ include/configs/verdin-imx95.h
@@ -51,7 +51,7 @@
 
 #define XEN_BOOT_ENV \
 	    "domu-android-auto=no\0" \
-            "xenhyper_bootargs=console=dtuart dom0_mem=2048M dom0_max_vcpus=2 \0" \
+            "xenhyper_bootargs=console=dtuart dom0_mem=4096M dom0_max_vcpus=2 \0" \
             "xenlinux_bootargs= \0" \
             "xenlinux_console=hvc0 earlycon=xen\0" \
             "xenlinux_addr=0x9e000000\0" \
